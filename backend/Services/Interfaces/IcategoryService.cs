1d2a2580970078e714c219d9742c7d36f843aea7
vikeshsurya chava <chavavikeshsurya@gmail.com>
2026-06-24 02:00:19 -0700

    Pushing solution

--- backend/Services/Interfaces/IcategoryService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace Patient_Grievance_and_Complaint_Resolution.Services.Interfaces
-{
-    public interface ICategoryService
-    {
-        Task<IActionResult> GetCategories();
-    }
-}
